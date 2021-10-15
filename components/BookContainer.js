2a6321cbe802b7eb4b69207542423588f2816fb5
Franz Srambical <franz.srambical@gmail.com>
2021-10-15 03:38:42 +0200

    commit

---
+++ components/BookContainer.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Text, Icon, Box, Pressable, gradColors, Center, Heading, Example, HStack, Spacer, Flex } from "native-base";
+export const BookContainer = (props) => {
+  return (
+    <Pressable
+      onPress={() => {
+        console.log("Hello world")
+      }}
+    >
+      <Box p="5" rounded="8" bg="cyan.700">
+        <HStack alignItems="flex-start">
+          <Text fontSize={12} color="cyan.50" fontWeight="medium">
+            Business
+          </Text>
+          <Spacer />
+          <Text fontSize={10} color="cyan.100">
+            1 month ago
+          </Text>
+        </HStack>
+        <Text color="cyan.50" mt="3" fontWeight="medium" fontSize={20}>
+          Marketing License
+        </Text>
+        <Text mt="2" fontSize={14} color="cyan.100">
+          Unlock powerfull time-saving tools for creating email delivery and
+          collecting marketing data
+        </Text>
+        <Flex>
+          <Text mt="2" fontSize={12} fontWeight="medium" color="cyan.400">
+            Read More
+          </Text>
+        </Flex>
+      </Box>
+    </Pressable>
+  );
+};
