35b9f336db7b17ca552ee7126f7c5d8651f06c2e
Franz Srambical <franz.srambical@gmail.com>
2021-10-15 04:12:28 +0200

    homescreen

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from 'react-native';
 import {
   Box,
@@ -31,45 +31,9 @@ const config = {
 // extend the theme
 export const theme = extendTheme({ config });
 
-function SearchBar() {
-  return (
-    <VStack
-      space={5}
-      width="100%"
-      divider={
-        <Box px="2">
-          <Divider />
-        </Box>
-      }>
-      <VStack width="100%" space={5} alignItems="center">
-        <Input
-          placeholder="Search"
-          variant="filled"
-          width="100%"
-          bg="gray.100"
-          borderRadius="10"
-          py="1"
-          px="2"
-          placeholderTextColor="gray.500"
-          _hover={{ bg: 'gray.200', borderWidth: 0 }}
-          borderWidth="0"
-          _web={{
-            _focus: { style: { boxShadow: 'none' } },
-          }}
-          InputLeftElement={
-            <Icon
-              ml="2"
-              size="5"
-              color="gray.500"
-              as={<Ionicons name="ios-search" />}
-            />
-          }
-        />
-      </VStack>
-    </VStack>
-  );
-}
 export default function App() {
+  const [searchText, setSearchText] = useState(null);
+
   return (
     <NativeBaseProvider>
       <Center
@@ -81,11 +45,52 @@ export default function App() {
         <VStack space={5} alignItems="center">
           <NativeBaseIcon />
           <Heading size="lg">Welcome to brif</Heading>
-          <SearchBar />
-          <HStack space={2} alignItems="center">
-            <Text>Search for a book to get its summary.</Text>
-          </HStack>
-          <BookContainer author='penis'/>
+
+          {/* Search */}
+          <VStack
+            space={5}
+            width="100%"
+            divider={
+              <Box px="2">
+                <Divider />
+              </Box>
+            }>
+            <VStack width="100%" space={5} alignItems="center">
+              <Input
+                placeholder="Search"
+                variant="filled"
+                width="100%"
+                bg="gray.100"
+                borderRadius="10"
+                py="1"
+                px="2"
+                placeholderTextColor="gray.500"
+                _hover={{ bg: 'gray.200', borderWidth: 0 }}
+                borderWidth="0"
+                _web={{
+                  _focus: { style: { boxShadow: 'none' } },
+                }}
+                onChangeText={setSearchText}
+                InputLeftElement={
+                  <Icon
+                    ml="2"
+                    size="5"
+                    color="gray.500"
+                    as={<Ionicons name="ios-search" />}
+                  />
+                }
+              />
+            </VStack>
+          </VStack>
+
+          {!searchText ?
+            <HStack space={2} alignItems="center">
+              <Text>Search for a book to get its summary.</Text>
+            </HStack>
+            :
+
+            <BookContainer author='Franz' title='The Game of Life' />
+          }
           <ToggleDarkMode />
         </VStack>
       </Center>
--- components/BookContainer.js
+++ components/BookContainer.js
@@ -2,34 +2,41 @@ import React from "react";
 import { Text, Icon, Box, Pressable, gradColors, Center, Heading, Example, HStack, Spacer, Flex } from "native-base";
 export const BookContainer = (props) => {
   return (
-    <Pressable
-      onPress={() => {
-        console.log("Hello world")
+    <Pressable>
+      {({ isHovered, isFocused, isPressed }) => {
+        return (
+          <Box
+            bg={isPressed ? "cyan.900" : isHovered ? "cyan.800" : "cyan.700"}
+            p="5"
+            rounded="8"
+            w="200"
+            style={{
+              transform: [
+                {
+                  scale: isPressed ? 0.96 : 1,
+                },
+              ],
+            }}
+          >
+            <HStack alignItems="flex-start">
+              <Text fontSize={12} color="cyan.50" fontWeight="medium">
+                {props.author}
+              </Text>
+              <Spacer />
+              <Text fontSize={10} color="cyan.100">
+                added 1 month ago
+              </Text>
+            </HStack>
+            <Text color="cyan.50" mt="3" fontWeight="medium" fontSize={20}>
+              {props.title}
+            </Text>
+            <Text mt="2" fontSize={14} color="cyan.100">
+              Unlock powerfull time-saving tools for creating email delivery and
+              collecting marketing data
+            </Text>
+          </Box>
+        )
       }}
-    >
-      <Box p="5" rounded="8" bg="cyan.700">
-        <HStack alignItems="flex-start">
-          <Text fontSize={12} color="cyan.50" fontWeight="medium">
-            Business
-          </Text>
-          <Spacer />
-          <Text fontSize={10} color="cyan.100">
-            1 month ago
-          </Text>
-        </HStack>
-        <Text color="cyan.50" mt="3" fontWeight="medium" fontSize={20}>
-          Marketing License
-        </Text>
-        <Text mt="2" fontSize={14} color="cyan.100">
-          Unlock powerfull time-saving tools for creating email delivery and
-          collecting marketing data
-        </Text>
-        <Flex>
-          <Text mt="2" fontSize={12} fontWeight="medium" color="cyan.400">
-            Read More
-          </Text>
-        </Flex>
-      </Box>
     </Pressable>
   );
 };
