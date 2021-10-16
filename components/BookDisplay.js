0bac39c95d42d11d18466c07839af0e3273ebb96
Mihir Mahajan <mihir.mahajan2002@gmail.com>
2021-10-16 19:51:14 +0000

    fetched data

--- components/BookDisplay.js
+++ components/BookDisplay.js
@@ -3,14 +3,14 @@ import { Heading, NativeBaseProvider, Center, Text } from 'native-base';
 
 export const BookDisplay = ({ route, navigation }) => {
 
-  const { author, title, year } = route.params;
+  const { name, sum} = route.params;
   return (
     <NativeBaseProvider>
       <Center >
-        <Heading> {title} - {author} </Heading>
+        <Heading>{name}</Heading>
         <Text>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-        </Text>
+         {sum} 
+       </Text>
       </Center>
     </NativeBaseProvider>
   );
